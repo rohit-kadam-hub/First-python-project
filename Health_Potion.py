d170a2722247a56ad22040fad182e07610d1045d
rohit-kadam-hub <67599998+rohit-kadam-hub@users.noreply.github.com>
2020-07-03 14:36:14 +0530

    Update Health_Potion.py

--- Health_Potion.py
+++ Health_Potion.py
@@ -1,1 +1,14 @@
-import 
+import random 
+health = 50
+potion_health =random.randint(25,50)
+import random
+
+health=50
+
+difficulty=2
+
+potion_health=int(random.randint(25,50)/difficulty)
+
+health=health+potion_health
+print(health)
+ 
